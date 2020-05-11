643ae74301811581e8f5148d55663a46b70746e8
Ryan Werner <rmwerner@umich.edu>
2020-05-11 16:42:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 function add(a,b) {return a + b}
-function subtract(a,b) {return `a - b`}
-function divide(a,b) {return `a/b`}
-function multiply(a,b) {return `a * b`}
-function increment(n) {return `n += 1`}
-function decrement(n) {return `n -= 1`}
-add(a,b)
+function subtract(a,b) {return a - b}
+function divide(a,b) {return a/b}
+function multiply(a,b) {return a * b}
+function increment(n) {return n += 1}
+function decrement(n) {return n -= 1}
