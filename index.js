65e7e4c05999be82469900989061215e370dd8f6
Ryan Werner <rmwerner@umich.edu>
2020-05-11 16:50:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function divide(a,b) {return a/b}
 function multiply(a,b) {return a * b}
 function increment(n) {return n += 1}
 function decrement(n) {return n -= 1}
-function makeInt(n) {return parseInt(`n`, 10)}
+function makeInt(n) {return parseInt(`n`)}
 function preserveDecimal(n) {return parseFloat('n')}
